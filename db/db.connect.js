1f06b5caef05a24a69dacc267c7466044db40927
Vikrant Patil <pvikrant248@gmail.com>
2026-06-21 15:30:14 +0530

    fixed MongoDB connection for Vercel serverless environment

--- db/db.connect.js
+++ db/db.connect.js
@@ -3,14 +3,23 @@ require("dotenv").config();
 
 const mongoUri = process.env.MONGODB;
 
+let isConnected = false;
+
 const initializeDatabase = async () => {
+  if (isConnected) {
+    console.log("Using existing connection");
+    return;
+  }
+
   await mongoose
     .connect(mongoUri)
     .then(() => {
+      isConnected = true;
       console.log("Connected to the Database");
     })
     .catch((error) => {
       console.log("Error connecting to the Database", error);
+      process.exit(1);
     });
 };
 
